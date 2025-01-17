87813cd07c562150bbf354cc0f27591b38eff51f
Natalya Koshukova <koshukovanat@gmail.com>
2025-01-17 23:09:50 +0700

    .

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
+// 14. Longest Common Prefix
+//Write a function to find the longest common prefix string amongst an array of strings.
+var longestCommonPrefix = function (strs) {
+    for (let i = 0; i < strs[0].length; i++) {
+        const char = strs[0][i];
+
+        for (let j = 1; j < strs.length; j++) {
+            if (i >= strs[j].length || strs[j][i] !== char) {
+                return strs[0].slice(0, i);
+            }
+        }
+    }
+    return strs[0];
+};
+console.log(longestCommonPrefix(["flower", "flow", "flight"])); // 'fl'
+console.log(longestCommonPrefix(["dog", "racecar", "car"])); // ''
+
+// 58. Length of Last Word
+//Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+//1 ок
+var lengthOfLastWord = function (s) {
+    let arr = s.trim().split(" ");
+
+    return arr[arr.length - 1].length;
+};
+console.log(lengthOfLastWord("   fly me   to   the moon  "));
+
+//2 оптимален
+var lengthOfLastWord = function (s) {
+    let lastWordLength = 0;
+    let i = s.length - 1;
+
+    // Пропускаем пробелы в конце строки
+    while (i >= 0 && s[i] === " ") {
+        i--;
+    }
+
+    // Считаем длину последнего слова
+    while (i >= 0 && s[i] !== " ") {
+        lastWordLength++;
+        i--;
+    }
+
+    return lastWordLength;
+};
+
 // 121. Best Time to Buy and Sell Stock
 //You are given an array prices where prices[i] is the price of a given stock on the ith day.You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 //1 Код имеет временную сложность O(n^2)
@@ -20,7 +67,7 @@ var maxProfit = function (prices) {
 console.log(maxProfit([7, 1, 5, 3, 6, 4])); //6-1=5
 console.log(maxProfit([7, 6, 4, 3, 1])); //0
 
-//2
+//2 OK
 var maxProfit = function (prices) {
     let minPrice = Infinity; //Инициализируем минимальную цену как бесконечность
     let maxProfit = 0;
@@ -34,15 +81,25 @@ var maxProfit = function (prices) {
 
 // 189. Rotate Array
 //Given an integer array nums, rotate the array to the right by k steps
+//1 не является оптимальным по производительности, особенно для больших массивов
 var rotate = function (nums, k) {
     for (let i = 0; i < k; i++) {
         const pop = nums.pop();
-        nums.unshift(pop);
+        nums.unshift(pop); //эта операция требует сдвига всех элементов массива вправо
     }
     return nums;
 };
 console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3)); //[5,6,7,1,2,3,4]
 console.log(rotate([-1, -100, 3, 99], 2)); //[3,99,-1,-100]
+
+//2 ОК
+var rotate = function (nums, k) {
+    const length = nums.length;
+    k = k % length; // Если k > n, то делаем k = k % n, чтобы уменьшить число сдвигов, k = остаток деления
+    nums.splice(0, 0, ...nums.splice(length - k));
+    // Ничего не удаляем, Извлекаем последние k элементов из массива и Вставляем в начало массива (по индексу 0).
+    return nums;
+};
 
 // 169. Majority Element
 //Given an array nums of size n, return the majority element. The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
